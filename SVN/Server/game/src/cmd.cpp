c9e7b6d17e6403911bd65be7eea82676f638e314
Licymnius <licymnius@outlook.com>
2021-10-17 09:07:35 +0300

    Update cmd.cpp

--- SVN/Server/game/src/cmd.cpp
+++ SVN/Server/game/src/cmd.cpp
@@ -10,11 +10,11 @@ ACMD(do_hardware_ban);
 
 //find again;
 
-	{ "do_clear_affect",		do_clear_affect,		0,			POS_DEAD,	GM_LOW_WIZARD	},
+			{ "do_clear_affect",		do_clear_affect,		0,			POS_DEAD,	GM_LOW_WIZARD	},
 
 //add below;
 
 #ifdef ENABLE_HWID_BAN_SYSTEM
-	{ "hw_ba",					do_inputall,			0,			POS_DEAD,	GM_IMPLEMENTOR	},
-	{ "hw_ban",					do_hardware_ban,		0,			POS_DEAD,	GM_IMPLEMENTOR	},
+			{ "hw_ba",			do_inputall,			0,			POS_DEAD,	GM_IMPLEMENTOR	},
+			{ "hw_ban",			do_hardware_ban,		0,			POS_DEAD,	GM_IMPLEMENTOR	},
 #endif
